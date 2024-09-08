d56a9a77a98bcd8f7af2ab2303a81aaa627cc814
eduardo2382 <motadossantosjoseeduardo.m82@gmail.com>
2024-09-08 17:10:04 -0300

    new html, css and js structure

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const btn = document.querySelector('button')
-const main = document.querySelector('main')
+const phrase = document.querySelector('.phrase')
 
 btn.addEventListener('click', ()=>{
     getPhrase()
 })
+
 
 async function getPhrase() {
     try{
@@ -11,22 +12,13 @@ async function getPhrase() {
 
         loadPhrase(phrase.quote)
     } catch(e){
-        main.innerHTML = ''
-        main.innerText = 'Sem frases'
+        phrase.innerHTML = ''
+        phrase.innerText = 'Sem frases'
     }
 }
 
-async function loadPhrase(phrase){
-    main.innerHTML = ''
-
-    let text = document.createElement('h1')
-
-    let author = document.createElement('p')
-    author.innerText = '-Kanye West'
-
-    text.innerText = `"${phrase}"`
-    main.appendChild(text)
-    main.appendChild(author)
+async function loadPhrase(text){
+    phrase.innerText = text
 
 }
 
