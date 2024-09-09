cb0fe38c4484838704dce70b5a03e11002f818ed
eduardo2382 <motadossantosjoseeduardo.m82@gmail.com>
2024-09-08 21:31:00 -0300

    language bar transition

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 const btn = document.querySelector('.btnNewPhrase')
 const phrase = document.querySelector('.phrase')
 
+const flagBr = document.querySelector('.br')
+const flagUsa = document.querySelector('.usa')
+var language = 'usa'
+
+flagBr.addEventListener('click', ()=>{
+    if(language != 'br'){
+        toggleLanguage()
+    }
+})
+
+flagUsa.addEventListener('click', ()=>{
+    if(language != 'usa'){
+        toggleLanguage()
+    }
+})
+
 btn.addEventListener('click', ()=>{
     getPhrase()
 })
+
+function toggleLanguage(){
+    let languageBar = document.querySelector(".languageBar")
+
+    if(language == 'usa'){
+        language = 'br'
+        languageBar.style.left = '10px'
+    } else{
+        language = 'usa'
+        languageBar.style.left = '46px'
+    }
+}
 
 
 async function getPhrase() {
