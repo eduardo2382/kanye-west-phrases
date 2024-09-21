5c0f1fc16eadaab1e357aba8d673c2c00a22e102
eduardo2382 <motadossantosjoseeduardo.m82@gmail.com>
2024-09-21 17:35:30 -0300

    page translation

--- script.js
+++ script.js
@@ -3,16 +3,28 @@ const phrase = document.querySelector('.phrase')
 
 const flagBr = document.querySelector('.br')
 const flagUsa = document.querySelector('.usa')
-var language = 'usa'
+var currentLanguage = 'en'
+const languages = [
+    {
+        language: 'pt-br',
+        btnNewPhrase: 'Nova Frase',
+        txtFooter: 'Feito por eduardo2382'
+    },
+    {
+        language: 'en',
+        btnNewPhrase: 'New Phrase',
+        txtFooter: 'Make by eduardo2382'
+    }
+]
 
 flagBr.addEventListener('click', ()=>{
-    if(language != 'br'){
+    if(currentLanguage != 'pt-br'){
         toggleLanguage()
     }
 })
 
 flagUsa.addEventListener('click', ()=>{
-    if(language != 'usa'){
+    if(currentLanguage != 'en'){
         toggleLanguage()
     }
 })
@@ -24,13 +36,24 @@ btn.addEventListener('click', ()=>{
 function toggleLanguage(){
     let languageBar = document.querySelector(".languageBar")
 
-    if(language == 'usa'){
-        language = 'br'
+    if(currentLanguage == 'en'){
+        currentLanguage = 'pt-br'
+        translationPage()
         languageBar.style.left = '10px'
     } else{
-        language = 'usa'
+        currentLanguage = 'en'
+        translationPage()
         languageBar.style.left = '46px'
     }
+}
+
+function translationPage(){
+    languages.forEach((item)=>{
+        if(item.language == currentLanguage){
+            btn.innerText = item.btnNewPhrase
+            document.querySelector('.textFooter').innerText = item.txtFooter
+        }
+    })
 }
 
 
